39cc86dc5fe32d96e0c81dde0e0557c734081fa7
Darren Henderson <darrenhusa@yahoo.com>
2021-05-20 15:04:51 -0500

    Test php-cs-fixer extension.

---
+++ app/Models/Foobar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Foobar extends Model
+{
+    public function name()
+    {
+        return 'Hey Jude';
+    }
+}
