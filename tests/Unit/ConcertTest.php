8a61fe08fecf4c22ce6ff1bc22ca6a357c522501
Darren Henderson <darrenhusa@yahoo.com>
2021-05-20 10:48:29 -0500

    Finish video 1.5 - Refactoring For Speed. Not seeing speed improvements in tests?

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -12,13 +12,13 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ConcertTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
 
     /** @test */
     public function can_get_formatted_date()
     {
     	$concert = Concert::factory()
-    		->create([
+    		->make([
     			'date' => Carbon::parse('2016-12-01 8:00pm'),
     	]);
 
@@ -26,9 +26,33 @@ class ConcertTest extends TestCase
 		// dump($concert);
 		// dd($concert);
 
-    	$date = $concert->formatted_date;
+    	// $date = $concert->formatted_date;
 
 
-		$this->assertEquals('December 1, 2016', $date);        
+		$this->assertEquals('December 1, 2016', $concert->formatted_date);        
     }
+
+	 /** @test */
+	 public function can_get_formatted_start_time()
+	 {
+		 $concert = Concert::factory()
+			 ->make([
+				 'date' => Carbon::parse('2016-12-01 17:00'),
+		 ]);
+ 
+ 
+		 $this->assertEquals('5:00pm', $concert->formatted_start_time);        
+	 }
+
+	  /** @test */
+	  public function can_get_ticket_price_in_dollars()
+	  {
+		  $concert = Concert::factory()
+			  ->make([
+				  'ticket_price' => 6750,
+		  ]);
+  
+  
+		  $this->assertEquals('67.50', $concert->ticket_price_in_dollars);        
+	  }
 }
