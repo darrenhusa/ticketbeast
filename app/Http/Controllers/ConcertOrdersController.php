2cfb1005461d2b0ecb41df35fe922330707b3c9e
Darren Henderson <darrenh@ccsj.edu>
2021-05-22 19:33:46 -0500

    Finish video 3.4 Finishing the Feature Test.

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Concert;
 use App\Billing\PaymentGateway;
 use App\Billing\PaymentFailedException;
+use App\Exceptions\NotEnoughTicketsException;
 use Illuminate\Http\Request;
 
 class ConcertOrdersController extends Controller
@@ -28,12 +29,13 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
-    
             $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
-        
+            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+            
             return response()->json([], 201);
         } catch (PaymentFailedException $e) {     
+            return response()->json([], 422);
+        } catch (NotEnoughTicketsException $e) {     
             return response()->json([], 422);
         }
 
--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -50,6 +50,7 @@ class PurchaseTicketsTest extends TestCase
     {
         
         $concert = Concert::factory()->published()->create(['ticket_price'  => 3250]);
+        $concert->addTickets(3);
 
         // dd($concert); -> works!
 
@@ -169,6 +170,7 @@ class PurchaseTicketsTest extends TestCase
         public function an_order_is_not_created_if_payment_fails()
         {
             $concert = Concert::factory()->published()->create();
+            $concert->addTickets(3);
             
             $response = $this->orderTickets($concert, [
                 'email' =>  'john@example.com', 
@@ -186,6 +188,7 @@ class PurchaseTicketsTest extends TestCase
         public function cannot_purchase_tickets_to_an_unpublished_concert()
         {
             $concert = Concert::factory()->unpublished()->create();
+            $concert->addTickets(3);
 
             $response = $this->orderTickets($concert, [
                 'email' =>  'john@example.com', 
