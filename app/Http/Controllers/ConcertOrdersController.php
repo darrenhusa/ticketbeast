e849470e64c4651480585d22f563488606044253
Darren Henderson <darrenh@ccsj.edu>
2021-05-22 20:45:48 -0500

    Finish video 3.5 Cancelling Failed Orders.

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -33,7 +33,8 @@ class ConcertOrdersController extends Controller
             $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
             
             return response()->json([], 201);
-        } catch (PaymentFailedException $e) {     
+        } catch (PaymentFailedException $e) {   
+            $order->cancel();  
             return response()->json([], 422);
         } catch (NotEnoughTicketsException $e) {     
             return response()->json([], 422);
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -9,6 +9,9 @@ class Ticket extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
+
     public function scopeAvailable($query)
     {
         return $query->whereNull('order_id');
