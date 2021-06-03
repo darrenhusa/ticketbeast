272a707b91c6c2ae785fbd11923f9cb0182c1241
Darren Henderson <darrenh@ccsj.edu>
2021-06-02 19:20:32 -0500

    Finish video 50 Refactoring Long Parameter List Using Preserve Whole Object.

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use App\Models\Concert;
 use App\Models\Order;
+use App\Models\Ticket;
+use App\Reservation;
 // use PHPUnit\Framework\TestCase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -26,7 +28,22 @@ class OrderTest extends TestCase
      $this->assertEquals(2, $concert->ticketsRemaining());
     }
 
-     /** @test */
+    /** @test */
+    public function creating_an_order_from_a_reservation()
+    {
+          $concert = Concert::factory()->create(['ticket_price' => 1200]);
+          $tickets = Ticket::factory(3)->create(['concert_id' => $concert->id]);
+
+          $reservation = new Reservation($tickets, 'john@example.com');
+          
+          $order = Order::fromReservation($reservation);
+
+          $this->assertEquals('john@example.com', $order->email);
+          $this->assertEquals(3, $order->ticketQuantity());
+          $this->assertEquals(3600, $order->amount);
+    }
+
+    /** @test */
      public function converting_to_an_array()
      {
           $concert = Concert::factory()->create(['ticket_price' => 1200])->addTickets(5);
